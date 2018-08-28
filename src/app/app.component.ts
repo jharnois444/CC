057b1515b9e1d8e1d292be090250827b793c53a1
Bolaji Olajide <bolaji.olajide@andela.com>
2018-08-28 02:21:44 +0100

    display random images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,7 @@ import { ApiService } from './api.service';
 })
 export class AppComponent {
   title = 'Capco-Challenge';
-  private images = [];
-  private imagesOne = [];
-  private imagesTwo = [];
-  private numItemsToGenerate = 8;
 
   constructor( private apiService: ApiService) {
-    this.apiService.getImages()
-      .then(res => {
-        this.images = res.json().slice(0, 16);
-        this.imagesOne = this.images.slice(0, 8);
-        this.imagesTwo = this.images.slice(8, 16);
-        console.log(this.imagesTwo);
-      });
   }
 }
